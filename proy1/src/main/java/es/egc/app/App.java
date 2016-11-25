8efb5bcf512a0ff2a9e6ed6972cb534f7d9af2e7
Alejandro <alex_sfc4@hotmail.com>
2016-11-25 09:11:41 +0100

    Primer proyecto maven

---
+++ proy1/src/main/java/es/egc/app/App.java
@@ -0,0 +1,17 @@
+package es.egc.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+	
+	static int getVal(){
+		return 1;
+	}
+}
